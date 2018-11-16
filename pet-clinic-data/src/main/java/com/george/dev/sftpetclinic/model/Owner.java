6df6d580e67ce3cea155330cc6d4f92656f26616
george <gokereka@teamapt.com>
2018-11-16 19:10:23 +0100

    Added index page and controller closes #8

--- pet-clinic-data/src/main/java/com/george/dev/sftpetclinic/model/Owner.java
+++ pet-clinic-data/src/main/java/com/george/dev/sftpetclinic/model/Owner.java
@@ -7,5 +7,5 @@ import javax.persistence.Entity;
  */
 
 @Entity
-public class Vet extends Person {
+public class Owner extends Person {
 }
---
+++ pet-clinic-web/src/main/java/com/george/dev/sftpetclinic/controllers/IndexController.java
@@ -0,0 +1,17 @@
+package com.george.dev.sftpetclinic.controllers;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+/**
+ * Created by George on 16/11/2018
+ */
+
+@Controller
+public class IndexController {
+
+    @RequestMapping({"","/","index", "index.html"})
+    public String index(){
+        return "index";
+    }
+}
