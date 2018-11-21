5ac695b5c6956d4a4e55dd6e58e87eef1c8c588f
george <gokereka@teamapt.com>
2018-11-21 19:01:24 +0100

    List all vets on Owner vets page. closes #22

--- pet-clinic-web/src/main/java/com/george/dev/sftpetclinic/controllers/VetController.java
+++ pet-clinic-web/src/main/java/com/george/dev/sftpetclinic/controllers/VetController.java
@@ -1,8 +1,10 @@
 package com.george.dev.sftpetclinic.controllers;
 
+import com.george.dev.data.services.VetService;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 /**
@@ -14,9 +16,17 @@ public class VetController {
 
     private final Logger logger = LoggerFactory.getLogger(getClass().getName());
 
+    private final VetService vetService;
+
+    public VetController(VetService vetService) {
+        this.vetService = vetService;
+    }
+
     @RequestMapping({"/vets","/vets/index", "/vets/index.html"})
-    public String listVets(){
+    public String listVets(Model model){
         logger.info("Calling list of Vets page");
+
+        model.addAttribute("vets", vetService.findAll());
         return "vets/vets-index";
     }
 
